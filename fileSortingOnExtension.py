1fc88db28e832388cb7cdd5aef6171be8a04fd43
Kush <kushagrachetan@gmail.com>
2018-02-12 15:03:58 +0530

    Added Extracting of extension form filename Added Sorting of Files based on extention

---
+++ fileSortingOnExtension.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+
+def findFileExtension(file_path):
+    for ext in ['.tar.gz', '.tar.bz2']:
+        if file_path.endswith(ext):
+            # Since these has 2 dots in extension and also removing the dot
+            return (file_path[:-len(ext)], file_path[-len(ext):])[1][1:]
+    return os.path.splitext(file_path)[1][1:]
+
+
+def sortOnExtension(sourceDirectory, destinationDirectory):
+    print("Here")
+    print(sourceDirectory)
+
+    for root, directories, filenames in os.walk(sourceDirectory):
+        print("Here")
+        for filename in filenames:
+            source = os.path.abspath(os.path.join(root, filename))
+
+            fileExtension = '/' + findFileExtension(source)
+            print(fileExtension)
+
+            if not os.path.isdir(source):
+                if not os.path.isdir(destinationDirectory + fileExtension):
+                    os.makedirs(destinationDirectory + fileExtension)
+                shutil.move(source, os.path.join(destinationDirectory + fileExtension, filename))
+
+
+if __name__ == "__main__":
+    sortOnExtension("/home/kush/Downloads/Mixed", "/home/kush/Downloads/Sorted")
